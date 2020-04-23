7bbb8f77d206f98b69d6cb8c5f7da8e50df3ea23
ErickRabpit <erickrabpit@gmail.com>
2020-04-23 14:55:54 -0400

    Tutorial_Competado
    Hasta el video 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CompileShallowModuleMetadata } from '@angular/compiler';
+import { UsuariosService } from './usuarios.service';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +11,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   mensaje = 'Curso de Angular';
   nombre = 'Luis';
-
+  botonStatus = true;
+  isActive = true;
   getNombre(){
     return this.nombre;
   }
 
   imagenURL = 'http://lorempixel.com/400/200/'
-  botonStatus = true;
-  isActive = true;
+  
+
+  save(e){
+    console.log(e);
+  }
+  onKeyUp(usuario){
+    console.log(usuario);
+  }
+
+  persona = {
+    nombre : 'Luis',
+    edad : 15
+  }
+
+  personas : any = [];
+  constructor(private usuarioservice : UsuariosService){
+    //let usuario = new UsuariosService();
+    this.personas = usuarioservice.getUsuarios();
+  }
+
+  miFormulario = new FormGroup({
+    username : new FormControl('', Validators.required),
+    userpassword : new FormControl('', Validators.required)
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { UsuariosService } from './usuarios.service';
+import { MensajeComponent } from './mensaje/mensaje.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
+    CursosComponent,
+    MensajeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    UsuariosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
